8794c2460f92db1c581b4f1ba69cb67364c3565f
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2024-12-01 19:47:20 +0000

    Add docstrings

--- include/advent_of_code/day_1.hxx
+++ include/advent_of_code/day_1.hxx
@@ -1,3 +1,13 @@
+/**
+ * @file day_1.hxx
+ * @author Kristian Zarebski (krizar312@gmail.com)
+ * @brief Methods for solutions to Day 1 of AOC24
+ * @version 0.1
+ * @date 2024-12-01
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #pragma once
 
 #include <algorithm>
@@ -15,7 +25,28 @@
 
 namespace AdventOfCode::Day1 {
   typedef std::pair<std::vector<int>, std::vector<int>> file_data;
+
+  /**
+   * @brief Read data from an input file containing historian location lists
+   * 
+   * @param input the input data file containing lists
+   * @return std::optional<file_data> the read data if successful
+   */
   std::optional<file_data> read_data(const std::filesystem::path& input);
+
+  /**
+   * @brief Get the differences between pair of ordered historian location lists
+   * 
+   * @param input the input data file containing lists
+   * @return std::vector<int> a vector of differences between the ordered lists
+   */
   std::vector<int> get_differences(const std::filesystem::path& input);
+
+  /**
+   * @brief Get the similarity scores for a pair of historian location lists
+   * 
+   * @param input the input data file containing lists
+   * @return std::vector<int> a vector of similarity scores for the two lists
+   */
   std::vector<int> get_similarity_scores(const std::filesystem::path& input);
 };
--- src/day_1.cxx
+++ src/day_1.cxx
@@ -4,6 +4,8 @@
 
 namespace AdventOfCode::Day1 {
   std::optional<file_data> read_data(const std::filesystem::path& input) {
+
+    // Attempt to read file, if the read fails return null object
     std::ifstream read_in(input, std::ios::in);
     file_data out{std::vector<int>(), std::vector<int>()};
 
@@ -12,20 +14,25 @@ namespace AdventOfCode::Day1 {
       return {};
     }
 
+    // Read the file line by line
     std::string line;
     std::string element;
 
     while (std::getline(read_in, line)) {
       std::istringstream stream(line);
 
+      // Whether to assign to the first or second vector
       bool is_first_col{true};
       
+      // For each line split by space to get elements
       for (std::string element; std::getline(stream, element, ' ');) {
 
+        // If the line is empty continue
         if(element.empty()) continue;
 
         const int num_element{std::stoi(element)};
 
+        // Assign to either the first or second vector
         std::vector<int>& column = is_first_col ? out.first : out.second;
 
         column.push_back(num_element);
@@ -35,6 +42,7 @@ namespace AdventOfCode::Day1 {
 
     }
 
+    // Sort both vectors/historian location lists
     std::sort(out.first.begin(), out.first.end());
     std::sort(out.second.begin(), out.second.end());
 
@@ -54,6 +62,8 @@ namespace AdventOfCode::Day1 {
 
     std::vector<int> differences(validated_data.first.size());
 
+    // Compare the two lists after sorting them getting the difference between
+    // the two numbers
     std::transform(
         validated_data.first.begin(),
         validated_data.first.end(),
@@ -78,6 +88,9 @@ namespace AdventOfCode::Day1 {
 
     std::vector<int> scores(validated_data.first.size());
 
+    // Iterate through both lists and either count the occurrences in the second list
+    // for each number in the first list. At the same time record the occurences so if
+    // there are any repeated values these can just be retrieved from cache
     std::transform(
         validated_data.first.begin(),
         validated_data.first.end(),
@@ -86,7 +99,11 @@ namespace AdventOfCode::Day1 {
           if(scoring_map.find(a) != scoring_map.end()) {
             return scoring_map.at(a);
           }
-          const int n_occurrences = std::count_if(validated_data.second.begin(), validated_data.second.end(), [a](int value){return value == a;});
+          const int n_occurrences = std::count_if(
+            validated_data.second.begin(),
+            validated_data.second.end(), 
+            [a](int value){return value == a;}
+          );
           scoring_map[a] = a * n_occurrences;
           return a * n_occurrences;
         }
