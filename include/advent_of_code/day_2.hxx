473ad9726398d7722eacc1bd97d9d94ec2c402f2
Kristian Zarębski <krizar312@gmail.com>
2024-12-02 07:27:58 +0000

    Indent fixes

--- include/advent_of_code/day_2.hxx
+++ include/advent_of_code/day_2.hxx
@@ -1,6 +1,13 @@
 #pragma once
 
+#include <sstream>
+#include <cstdlib>
+#include <string>
+#include <filesystem>
+#include <fstream>
+
 namespace AdventOfCode24::Day2 {
     const int BUFFER_SIZE{1024};
-    bool is_safe(std::istringstream line);
+    bool is_safe(std::istringstream& line);
+    bool check_reactor_safety(const std::filesystem::path& input_file);
 };
--- src/day_2.cxx
+++ src/day_2.cxx
@@ -1,31 +1,44 @@
-#include "advenr_of_code/day_2.hxx"
+#include "advent_of_code/day_2.hxx"
 
 namespace AdventOfCode24::Day2 {
     bool safe_pair(int val_1, int val_2, int direction) {
-	const int interval{val_2 - val_1};
-	const int abs_interval{std::abs(interval)};
+        const int interval{val_2 - val_1};
+        const int abs_interval{std::abs(interval)};
 
-        
+        if(abs_interval > 3 || abs_interval < 1) return false;
+
         if(direction > 0 && val_2 - val_1 <= 0) {
-	  return false;
-	}
-       	else if(direction < 0 && val_2 - val_1 >= 0) {
-          return false;
-	}
+	        return false;
+	    }
+        else if(direction < 0 && val_2 - val_1 >= 0) {
+             return false;
+	    }
+
+        return true;
     }
-    bool level_is_safe(std::istringstream line) {
-       std::string element;
-       int previous{-1};
-       int direction{0};
+    bool level_is_safe(std::istringstream& line) {
+        std::string element;
+        int previous{-1};
+        int direction{0};
 
 
-       while(std::getline(line, element, ' ')) {
+        while(std::getline(line, element, ' ')) {
             if(element.empty()) continue;
 
-	    const int number{std::stoi(element)};
-	    
-	    if(previous > -1) {
-	        if(direction > 
-       }
+	        const int number{std::stoi(element)};
+
+            if(previous > 0) {
+                if(!safe_pair(previous, number, direction)) return false;
+                if(direction == 0) direction = number - previous;
+            }
+            previous = number;
+        }
+
+        return true;
+    }
+
+    bool check_reactor_safety(const std::filesystem::path& input_file) {
+        // Attempt to read file, if the read fails return null object
+        std::ifstream read_in(input_file, std::ios::in);
     }
 };
